a906f9ba05b2443bbf41705176f38195fc3ad9f3
Ethorbit <Ethorbit@protonmail.com>
2023-06-20 04:15:42 -0700

    Add exception handling for API request

--- spore_random_creation_dl.py
+++ spore_random_creation_dl.py
@@ -50,62 +50,65 @@ def download_batch():
   
     request_data['httpSessionId'] = http_session_id
     request_data['scriptSessionId'] = script_session_id
-  
-    response = requests.post(
-        api_url, 
-        data = request_data, 
-        headers = { 'Content-Type': 'text/javascript;charset=utf-8' },
-        cookies = cookie_jar
-    )
-
-    cookie_jar = response.cookies
     
-    # Extract keys and values from the API's DWR response and assemble it into an easily parseable array:
-    dwr_response_segments = response.text.split(';')
-    dwr_data              = {}
-    dwr_last_element_data = {}
-    dwr_last_element_id   = None
-    for i in range(0, len(dwr_response_segments), 1):
-        key_value_pairs = dwr_response_segments[i].split('=')
-        if (len(key_value_pairs) >= 2):
-            key   = key_value_pairs[0]
-            value = key_value_pairs[1]
-            element_id_match = re.search('([a-z][0-9]*\.)', key)
-            if (element_id_match):
-                element_id = element_id_match.group()
-                key = key.replace(element_id_match.group(), '')
-                
-                # Check if we've iterated to a new item and act accordingly
-                if (element_id != dwr_last_element_id):
-                    if ('id' in dwr_last_element_data):
-                        dwr_data[dwr_last_element_data['id']] = dwr_last_element_data
+    try:
+        response = requests.post(
+            api_url, 
+            data = request_data, 
+            headers = { 'Content-Type': 'text/javascript;charset=utf-8' },
+            cookies = cookie_jar
+        )
+
+        cookie_jar = response.cookies
+        
+        # Extract keys and values from the API's DWR response and assemble it into an easily parseable array:
+        dwr_response_segments = response.text.split(';')
+        dwr_data              = {}
+        dwr_last_element_data = {}
+        dwr_last_element_id   = None
+        for i in range(0, len(dwr_response_segments), 1):
+            key_value_pairs = dwr_response_segments[i].split('=')
+            if (len(key_value_pairs) >= 2):
+                key   = key_value_pairs[0]
+                value = key_value_pairs[1]
+                element_id_match = re.search('([a-z][0-9]*\.)', key)
+                if (element_id_match):
+                    element_id = element_id_match.group()
+                    key = key.replace(element_id_match.group(), '')
                     
-                    dwr_last_element_id = element_id
-                    dwr_last_element_data = {}
-                else:
-                    dwr_last_element_data[key] = value
-   
-    # Print and download each creation:
-    for id, data in dwr_data.items():
-        if ('avatarImage' in data):
-            image_pattern = re.compile(r'(thumb)')
-            image_match = image_pattern.search(data['avatarImage'])
-            if (image_match):
-                # Format avatarImage into a valid downloadable image
-                image = data['avatarImage'].replace(image_match.group(), '').replace('\\', '').replace('\"', '')
-                image_extension_match = re.search(r'(\.[A-z]*)$', image)
-                if (image_extension_match):
-                    image_extension = image_extension_match.group()
-                    image = image.replace(image_extension, f'_lrg{image_extension}')
-                    image_url = static_img_url + image
-                    print(json.dumps(data, indent=4))
-                    print(f'Downloading from {image_url}')
-                    # TODO: check if this is a valid URL as currently this will pretend to download even if it's invalid
-                    try:
-                        urllib.request.urlretrieve(image_url, f'{args.dir}/{id}{image_extension}')
-                    except Exception as err:
-                        print(f'There was an error downloading that. - {err}')
-                    
+                    # Check if we've iterated to a new item and act accordingly
+                    if (element_id != dwr_last_element_id):
+                        if ('id' in dwr_last_element_data):
+                            dwr_data[dwr_last_element_data['id']] = dwr_last_element_data
+                        
+                        dwr_last_element_id = element_id
+                        dwr_last_element_data = {}
+                    else:
+                        dwr_last_element_data[key] = value
+    
+        # Print and download each creation:
+        for id, data in dwr_data.items():
+            if ('avatarImage' in data):
+                image_pattern = re.compile(r'(thumb)')
+                image_match = image_pattern.search(data['avatarImage'])
+                if (image_match):
+                    # Format avatarImage into a valid downloadable image
+                    image = data['avatarImage'].replace(image_match.group(), '').replace('\\', '').replace('\"', '')
+                    image_extension_match = re.search(r'(\.[A-z]*)$', image)
+                    if (image_extension_match):
+                        image_extension = image_extension_match.group()
+                        image = image.replace(image_extension, f'_lrg{image_extension}')
+                        image_url = static_img_url + image
+                        print(json.dumps(data, indent=4))
+                        print(f'Downloading from {image_url}')
+                        # TODO: check if this is a valid URL as currently this will pretend to download even if it's invalid
+                        try:
+                            urllib.request.urlretrieve(image_url, f'{args.dir}/{id}{image_extension}')
+                        except Exception as err:
+                            print(f'There was an error downloading that. - {err}')
+    except Exception as err:
+        print(f'Error trying to fetch creations from the Sporepedia API. - {err}')
+  
     # TODO: add check for downloads exceeding args.amount during thumbnail download loop
     
     time.sleep(timeout_secs)
